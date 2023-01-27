060c6a7b857cb987a26108dd4e8415d6d8318b75
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-27 15:58:23 +0200

    T03

---
+++ T03. Custom Min Function/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+HashSet<int> numbersSet = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(n => int.Parse(n))
+        .ToHashSet();
+
+Func<HashSet<int>, int> returnMinNumber = numbers => numbers.Min();
+
+
+Console.WriteLine(returnMinNumber(numbersSet));
