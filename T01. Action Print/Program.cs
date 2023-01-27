d475f1473c61127e79ce81e7d96255eee43c57b1
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-27 14:17:58 +0200

    Add project files.

---
+++ T01. Action Print/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Action<string[]> printNames = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+printNames(names);
