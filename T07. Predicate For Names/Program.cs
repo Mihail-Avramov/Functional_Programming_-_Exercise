bf096d2599df0bcb4a58dcfbd73bf61fcf5e1ee0
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-27 18:03:15 +0200

    T07

---
+++ T07. Predicate For Names/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int length = int.Parse(Console.ReadLine());
+
+List<string> namesList = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Action<List<string>, Predicate<string>> printFilteredList = (names, match) =>
+{
+    foreach (var name in names)
+    {
+        if (match(name))
+        {
+            Console.WriteLine(name);
+        }
+    }
+};
+
+Predicate<string> match = s => s.Length <= length;
+
+printFilteredList(namesList,match);
