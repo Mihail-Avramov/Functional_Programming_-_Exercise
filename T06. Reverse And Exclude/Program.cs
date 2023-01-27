d143d5a9c46784095335fa0b8c1eec35ca07605d
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-27 17:49:16 +0200

    T06

---
+++ T06. Reverse And Exclude/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int divider = int.Parse(Console.ReadLine());
+
+Func<List<int>, List<int>> reverseArrayFunc = numbers =>
+{
+    List<int> result = new List<int>();
+
+    for (int i = numbers.Count - 1; i >= 0; i--)
+    {
+        result.Add(numbers[i]);
+    }
+
+    return result;
+};
+
+
+Func<List<int>, Predicate<int>, List<int>> removeNumbersFunc = (numbers, match) =>
+{
+    List<int> result = new List<int>();
+
+    foreach (var number in numbers)
+    {
+        if (match(number))
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+};
+
+Predicate<int> match = n => n % divider != 0;
+
+numbers = removeNumbersFunc(numbers, match);
+
+numbers = reverseArrayFunc(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
