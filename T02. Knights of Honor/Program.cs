7269ef813c5fb8d01c020ed0435ac9b39a7977e3
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-27 15:48:15 +0200

    T02

---
+++ T02. Knights of Honor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Action<string, string[]> printNames = (title, names) =>
+{
+    foreach (var name in names)
+    {
+        Console.WriteLine($"{title} {name}");
+    }
+};
+
+string addTitle = "Sir";
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+printNames(addTitle, names);
