0aaf950301d9e84526250b345134860463033435
Mihail Avramov <115155730+Mihail-Avramov@users.noreply.github.com>
2023-01-29 12:20:28 +0200

    T11

---
+++ T11. TriFunction/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+int sum = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Func<string, int, bool> checkStringSum = (name, sum) => name.Sum(ch => ch) >= sum;
+
+Func<string[],int, Func<string, int, bool>, string> getFirstNameMatch = 
+    (names, sum, match) => names.FirstOrDefault(name => match(name, sum));
+
+Console.WriteLine(getFirstNameMatch(names, sum, checkStringSum));
